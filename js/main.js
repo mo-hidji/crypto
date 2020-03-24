de3e3e7284af848a71796a66c242760576b37bc1
Mohammed Hidji <medhidji1998@gmail.com>
2020-03-24 17:14:58 +0100

    enhanced content, enhanced menu, added content

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ function openMenu() {
     document.getElementById('mobile').style.opacity="1";
 };
 
-// slider
+// parners slider
 
 $(function() {
     
@@ -46,6 +46,49 @@ $('.partners').slick({
     autoplaySpeed: 1000
 });
 })
+
+// we can slider
+
+$('.sliders').slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 700,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    pauseOnFocus: false,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 790,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+// aos animation
 
 $(function() {
     AOS.init();
